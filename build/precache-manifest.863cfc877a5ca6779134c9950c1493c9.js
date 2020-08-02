5455b3b122d0a80b3190e17aa85fa9e92f332ec3
West Wong <westwong.0622@gmail.com>
2020-08-02 21:37:17 +0800

    first commit

---
+++ build/precache-manifest.863cfc877a5ca6779134c9950c1493c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfff672330cfa73b50c2e91d199d0fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd51c08fafcfff52ea04",
+    "url": "/static/css/main.8d3333de.chunk.css"
+  },
+  {
+    "revision": "4474e4ecf51a749be315",
+    "url": "/static/js/2.ff740746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff740746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd51c08fafcfff52ea04",
+    "url": "/static/js/main.c06107fa.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  }
+]);
