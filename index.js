e47fc8c27668e8524c95f01dc5528ca8992a7fbd
West Wong <westwong.0622@gmail.com>
2020-08-07 15:46:30 +0800

    update get id with wrong id

--- index.js
+++ index.js
@@ -52,9 +52,18 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
-  Note.findById(id).then(note => {
-    res.json(note)
-  })
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).end()
+    })
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
