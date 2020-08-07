c8c9484c5b53c16430cd495fad2ad87edd57aaa6
West Wong <westwong.0622@gmail.com>
2020-08-07 14:57:01 +0800

    noteSchema.set('toJSON', {transform})

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Note = mongoose.model('Note', noteSchema)
 
 const requestLogger = (request, response, next) => {
