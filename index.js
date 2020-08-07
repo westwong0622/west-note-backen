6650926711f319e6230dc1af37d74a8f38784a0b
West Wong <westwong.0622@gmail.com>
2020-08-07 15:03:02 +0800

    put code in models/note.js

--- index.js
+++ index.js
@@ -4,32 +4,8 @@ const cors = require('cors')
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
-const mongoose = require('mongoose')
 
-if ( process.argv.length < 3 ) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-const url = `mongodb+srv://note-data:${password}@cluster0.yeiup.mongodb.net/note-app?retryWrites=true&w=majority`
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
