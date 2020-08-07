8c3d567578f582042d84ef480e1995295e33fc33
West Wong <westwong.0622@gmail.com>
2020-08-07 14:34:22 +0800

    mongoose.connect(url) note.save().then(=>)

---
+++ mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length < 3 ) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://note-data:${password}@cluster0.yeiup.mongodb.net/test?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('Note saved!')
+  mongoose.connection.close()
+})
