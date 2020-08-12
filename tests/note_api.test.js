899e38965984dffe9d9c6220062736759db700fd
West Wong <westwong.0622@gmail.com>
2020-08-12 17:42:05 +0800

    test('note without content is not added'

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -29,26 +29,38 @@ beforeEach(async () => {
   await noteObject.save();
 });
 
-test('a valid note can be added', async () => {
+test('note without content is not added', async () => {
   const newNote = {
-    content: 'async/await simplifies making async calls',
     important: true,
-    date: new Date(),
   };
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+  await api.post('/api/notes').send(newNote).expect(400);
 
   const response = await api.get('/api/notes');
 
-  const contents = response.body.map((r) => r.content);
-
-  expect(response.body).toHaveLength(initialNotes.length + 1);
-  expect(contents).toContain('async/await simplifies making async calls');
+  expect(response.body).toHaveLength(initialNotes.length);
 });
+
+// test('a valid note can be added', async () => {
+//   const newNote = {
+//     content: 'async/await simplifies making async calls',
+//     important: true,
+//     date: new Date(),
+//   };
+
+//   await api
+//     .post('/api/notes')
+//     .send(newNote)
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/);
+
+//   const response = await api.get('/api/notes');
+
+//   const contents = response.body.map((r) => r.content);
+
+//   expect(response.body).toHaveLength(initialNotes.length + 1);
+//   expect(contents).toContain('async/await simplifies making async calls');
+// });
 
 // test('notes are returned as json', async () => {
 //   await api
