c338f455db707f09199a182d6b47e8b025cfe15f
West Wong <westwong.0622@gmail.com>
2020-08-12 15:54:48 +0800

    add two test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -11,6 +11,18 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes');
+
+  expect(response.body).toHaveLength(2);
+});
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes');
+
+  expect(response.body[0].content).toBe('HTML is easy');
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
