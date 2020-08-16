092f23eb52d8a86abbc53e8e4b39ab4231bb88f0
West Wong <westwong.0622@gmail.com>
2020-08-16 17:50:27 +0800

    add user in model note

--- models/note.js
+++ models/note.js
@@ -15,6 +15,10 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 noteSchema.set('toJSON', {
