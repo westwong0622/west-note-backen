7f89160bdf3dda5237874614d3616bc2bdb61b9f
West Wong <westwong.0622@gmail.com>
2020-08-16 18:37:38 +0800

    api/users

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
 usersRouter.post('/', async (request, response) => {
   const { body } = request;
   const saltRounds = 10;
