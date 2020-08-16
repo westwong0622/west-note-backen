9c297b05b4fef835447458f9e4ac5e1e056fc005
West Wong <westwong.0622@gmail.com>
2020-08-16 18:58:20 +0800

    add id in user

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({});
@@ -18,6 +20,8 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const { body } = request;
 
+  const user = await User.findById(body.userId);
+
   if (body.content === undefined) {
     return request.status(400).json({
       error: 'content missing',
@@ -28,9 +32,13 @@ notesRouter.post('/', async (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.json(savedNote);
   return null;
 });
