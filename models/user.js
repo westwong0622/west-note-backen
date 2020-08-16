0d6cd9383ad1eb11508f3cc7de74b8af40b514c4
West Wong <westwong.0622@gmail.com>
2020-08-16 17:50:15 +0800

    add model user

---
+++ models/user.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'note',
+    },
+  ],
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
