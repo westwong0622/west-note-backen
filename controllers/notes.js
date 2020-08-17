7f32d1f80a82b86600ac49f853ad4f78a5e7ec74
West Wong <westwong.0622@gmail.com>
2020-08-17 23:46:16 +0800

    verify token from request

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,6 +3,8 @@ const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
 
+const jwt = require('jsonwebtoken');
+
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
   response.json(notes);
@@ -17,10 +19,24 @@ notesRouter.get('/:id', async (request, response) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 notesRouter.post('/', async (request, response) => {
   const { body } = request;
 
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (body.content === undefined) {
     return request.status(400).json({
