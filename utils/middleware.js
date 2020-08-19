de4f968933e54fcf336d356f0fa7c35e9868bc8f
West Wong <westwong.0622@gmail.com>
2020-08-19 20:27:15 +0800

    if (error.name === 'JsonWebTokenError')

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,6 +21,11 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'Invalid token',
+    });
+  }
 
   next(error);
   return null;
