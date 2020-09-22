8b5211b7c00d9a799c3d8dfc86c478dc1fb73ab0
West Wong <westwong.0622@gmail.com>
2020-09-22 23:18:12 +0800

    reorder

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-underscore-dangle */
+const jwt = require('jsonwebtoken');
+
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
-
-const jwt = require('jsonwebtoken');
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
