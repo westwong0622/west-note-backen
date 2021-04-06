117f108a3dd61eacaefee963fafd1efaa4308790
West Wong <westwong.0622@gmail.com>
2021-04-07 00:27:34 +0800

    format

--- mongo.js
+++ mongo.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-if ( process.argv.length < 3 ) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
 }
 
-const password = process.argv[2]
-const url = `mongodb+srv://note-data:${password}@cluster0.yeiup.mongodb.net/note-app?retryWrites=true&w=majority`
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+const password = process.argv[2];
+const url = `mongodb+srv://note-data:${password}@cluster0.yeiup.mongodb.net/note-app?retryWrites=true&w=majority`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
   content: 'HTML is Easy',
   date: new Date(),
   important: true,
-})
+});
 
 // note.save().then(result => {
 //   console.log('Note saved!')
 //   mongoose.connection.close()
 // })
 
-Note.find({}).then(result => {
-  result.forEach(note => {
+Note.find({}).then((result) => {
+  result.forEach((note) => {
     console.log(note);
-  })
-  mongoose.connection.close()
-})
+  });
+  mongoose.connection.close();
+});
